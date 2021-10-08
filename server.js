d7873499e015c01e73a57ef27f6fcd0c004c0567
AndiCrop <andrescamargo.co@hotmail.com>
2021-10-07 21:38:20 -0500

    se agregó conexión a BD y rutas Patch y Delete

--- server.js
+++ server.js
@@ -1,24 +1,43 @@
 import Express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import Cors from 'cors';
 
-const app = Express();
-app.use(Express.json());
+const stringConexion = '';
 
-//ruta tipo get
-app.get('/Productos', (req, res) => {
-    console.log('Alguien hizo un get en la ruta /Productos');
-    const productos = [
-        { codigo: "A3020", descripcion: "Licra deportiva", valorunit: "$90.000",  estado: "Disponible" },
-        { codigo: "B4560", descripcion: "body deportivo", valorunit: "$50.000",  estado: "Disponible"  },
-        { codigo: "H9841", descripcion: "Joguer", valorunit: "$50.000",  estado: "Disponible"  },
-        { codigo: "E9618", descripcion: "Leggins", valorunit: "$80.000",  estado: "Disponible"  }
-        
-    ];
-    res.send(productos);
+
+const client = new MongoClient(stringConexion, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-app.post('/Productos/Nuevo', (req, res) => {
-    const datosProducto = req.body;
-    console.log("Llaves: ", Object.keys(datosProducto));
+
+let baseDeDatos;
+
+const app = Express();
+
+app.use(Express.json());
+app.use(Cors()); 
+
+
+app.get('/Productos', (req, res)=>{
+    console.log('Alguien hizo un get en la ruta /Productos');
+    baseDeDatos
+    .collection('Productos')
+    .find()
+    .limit(50)
+    .toArray((err, result)=>{
+        if (err){
+            res.status(500).send('error consultando Productos');
+        } else{
+            res.json(result);
+        }
+    });
+});
+
+
+app.post('/Productos/Nuevo', (req, res)=>{
+    const datosProducto = req.body; 
+    console.log('llaves: ', Object.keys(datosProducto)); 
     try{
         if( 
             Object.keys(datosProducto).includes("codigo") &&
@@ -26,8 +45,17 @@ app.post('/Productos/Nuevo', (req, res) => {
             Object.keys(datosProducto).includes("valorunit") && 
             Object.keys(datosProducto).includes("estado") 
         ) {
-            res.sendStatus(200); // enviar respuesta "OK"
-        } else{
+            baseDeDatos.collection('Productos').insertOne(datosProducto, (err, result)=>{ 
+                if(err){
+                    console.error(err);
+                    res.sendStatus(500);
+                    
+                } else {
+                    console.log(result);
+                    res.sendStatus(200);
+                }
+            }); 
+        } else {
             res.sendStatus(500);
         }
     } catch {
@@ -35,7 +63,57 @@ app.post('/Productos/Nuevo', (req, res) => {
     }
 });
 
-
-app.listen(5000, () => {
-    console.log('Escuchando el puerto 5000');
+app.patch('/Productos/Editar', (req, res)=>{
+    const edicion = req.body;
+    console.log(edicion);
+    const filtroProducto = { _id: new ObjectId(edicion.id)};
+    delete edicion.id;   
+    const operacion = {   
+        $set: edicion,
+    };
+    baseDeDatos
+    .collection('Productos')
+    .findOneAndUpdate(
+        filtroProducto, 
+        operacion, 
+        { upsert: true, returnOriginal: true },
+        (err, result)=>{
+            if(err){
+                console.error('error actualizando producto: ', err);
+                res.sendStatus(500);
+            }else{
+                console.log('actualizado con éxito');
+                res.sendStatus(200);
+            }
+        });
 });
+
+app.delete('/Productos/Eliminar', (req, res) =>{
+    const filtroProducto = { _id: new ObjectId(req.body.id)};
+    baseDeDatos.collection('Productos').deleteOne(filtroProducto, (err, result)=>{
+        if(err){
+            console.error(err);
+            res.sendStatus(500);
+        }else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+const main = () => {
+    client.connect((err,db)=>{
+        if(err){
+            console.error('error conectando a BD');
+            return 'error';
+        }
+        baseDeDatos =  db.db('FitnesShop'); 
+        console.log('BaseDeDatos exitosa');
+        return app.listen(5000, () => {
+            console.log('Escuchando el puerto 5000');
+        });
+    });
+    
+};
+
+main();
