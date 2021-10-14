0d1df61293b775570c9ba999793a5c322ad05e4d
paolaAndrea5 <you@example.com>
2021-10-14 17:47:53 -0500

    Autenticacion

--- server.js
+++ server.js
@@ -5,13 +5,30 @@ import { connectServer } from './db/db.js';
 import rutasUsuario from './views/user/userRoute.js';
 import rutasProducto from './views/product/productRoute.js';
 import rutasVenta from './views/sale/saleRoute.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+
+
 
 dotenv.config({ path: './.env' });
 
 const app = Express();
-
 app.use(Cors());
 app.use(Express.json());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://skylabtic.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-autenticacion',
+issuer: 'https://skylabtic.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 app.use(rutasUsuario);
 app.use(rutasProducto);
 app.use(rutasVenta);
